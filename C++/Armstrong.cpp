1eb3f768200e5029b81fac541efcfffba199dd86
VISHNU KT <37811932+vishnukt@users.noreply.github.com>
2019-10-26 22:53:22 +0530

    Amstrong.cpp using for loop
    Amstrong number checking using for loop in a single line

--- C++/Armstrong.cpp
+++ C++/Armstrong.cpp
@@ -6,11 +6,7 @@ int arm=0,a,b,c,d,no;
 cout << "Enter any number" << endl;
 cin >> no;
 d = no;
-while( no!= 0){
-    a=no%10;
-    arm+=a*a*a;
-    no=no/10;
-}
+for(d=no;no!= 0;a=no%10,arm+=a*a*a,no=no/10);
 if(arm==d)
   cout<<"Yes the number is an armstrong number"<<endl;
 else
